069640645f416a94beef73a4fc6473c4523bca60
orange <62349354+Orange-742@users.noreply.github.com>
2022-03-20 22:16:26 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace SearchOfExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.SearchWord = new System.Windows.Forms.TextBox();
+            this.labelBox = new System.Windows.Forms.ComboBox();
+            this.lab = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.FILENAME = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Show = new System.Windows.Forms.TextBox();
+            this.end = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(233, 125);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "搜索";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // SearchWord
+            // 
+            this.SearchWord.Location = new System.Drawing.Point(32, 125);
+            this.SearchWord.Name = "SearchWord";
+            this.SearchWord.Size = new System.Drawing.Size(181, 21);
+            this.SearchWord.TabIndex = 3;
+            // 
+            // labelBox
+            // 
+            this.labelBox.FormattingEnabled = true;
+            this.labelBox.Location = new System.Drawing.Point(114, 61);
+            this.labelBox.Name = "labelBox";
+            this.labelBox.Size = new System.Drawing.Size(194, 20);
+            this.labelBox.TabIndex = 4;
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab.Location = new System.Drawing.Point(29, 62);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(71, 16);
+            this.lab.TabIndex = 6;
+            this.lab.Text = "选择类别";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "选择excel文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FILENAME
+            // 
+            this.FILENAME.AutoSize = true;
+            this.FILENAME.Location = new System.Drawing.Point(133, 28);
+            this.FILENAME.Name = "FILENAME";
+            this.FILENAME.Size = new System.Drawing.Size(65, 12);
+            this.FILENAME.TabIndex = 19;
+            this.FILENAME.Text = "未选择文件";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label10.Location = new System.Drawing.Point(29, 95);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(119, 16);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "输入搜索的词：";
+            // 
+            // Show
+            // 
+            this.Show.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Show.Location = new System.Drawing.Point(326, 12);
+            this.Show.Multiline = true;
+            this.Show.Name = "Show";
+            this.Show.ReadOnly = true;
+            this.Show.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Show.Size = new System.Drawing.Size(292, 336);
+            this.Show.TabIndex = 21;
+            // 
+            // end
+            // 
+            this.end.Location = new System.Drawing.Point(233, 166);
+            this.end.Name = "end";
+            this.end.Size = new System.Drawing.Size(75, 23);
+            this.end.TabIndex = 22;
+            this.end.Text = "关闭";
+            this.end.UseVisualStyleBackColor = true;
+            this.end.Click += new System.EventHandler(this.end_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 360);
+            this.ControlBox = false;
+            this.Controls.Add(this.end);
+            this.Controls.Add(this.Show);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.FILENAME);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lab);
+            this.Controls.Add(this.labelBox);
+            this.Controls.Add(this.SearchWord);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Excel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox SearchWord;
+        private System.Windows.Forms.ComboBox labelBox;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label FILENAME;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox Show;
+        private System.Windows.Forms.Button end;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+//必要性更新
+//内容显示还能优化，更换为别的，输出一大块信息
+//没有打开文件前不能按搜索√
+//自动杀进程，不需要按关闭
+//显示下一个
+
+//花里胡哨更新
+//显示还有多少单元格没搜索
+//
+namespace SearchOfExcel
+{
+    public partial class Form1 : Form
+    {
+        //全局变量
+        Excel.Application excel;
+        Excel.Workbook xBook;
+        char Zimu;
+        int Label_Row;
+        int find;
+        string Search;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //初始化部分
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            excel = new Excel.Application();
+            Start.Enabled = false;
+        }
+        //选择文件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Zimu = Convert.ToChar('A' - 1);
+            Label_Row = 1;
+            GetPath();
+            Start.Enabled = true;
+        }
+        //搜索按键
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (Zimu != labelBox.Text[0] || Search != SearchWord.Text)
+            {
+                Zimu = labelBox.Text[0];
+                Search = SearchWord.Text;
+                find = 0;
+            }
+            Searching(excel, xBook, Zimu, Label_Row, Search);
+        }
+        //关闭按键
+        private void end_Click_1(object sender, EventArgs e)
+        {
+            EndOfExcel(excel, xBook);
+            this.Close();
+        }
+
+
+        //各种子函数
+        //选择文件路径
+        private void GetPath()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择文件夹";
+            dialog.Filter = "Excel文件(*.xlsx*)|*.xlsx*";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = dialog.FileName;
+                try
+                {
+                    xBook = excel.Workbooks._Open(@file);
+                    FILENAME.Text = file;
+                    GetLabel(excel, Zimu, ref Label_Row);
+                }
+                catch
+                {
+                    MessageBox.Show("该文件不是excel文件\n请重新选择" ,"警告");
+                }
+            }
+        }
+        //获取标签
+        private void GetLabel(Excel.Application excel, char Zimu, ref int number)
+        {
+            int end = 0;
+            labelBox.Items.Clear();
+            while (end <= 3)
+            {
+                Zimu++;
+                string position = Zimu + Convert.ToString(number);
+                Excel.Range rng1 = excel.get_Range(position, Type.Missing);
+                //单元格为空
+                if (rng1.Value2 == null)
+                {
+                    end++;
+                    if (end == 2 && Zimu <= 'C')//过早为null，怀疑是标题
+                    {
+                        //判断是否是合并单元格
+                        rng1 = (Excel.Range)excel.Cells[1, "A"];
+                        bool isMerge = (bool)rng1.MergeCells;
+                        if (isMerge)
+                        {
+                            //是则判断下一行是否为标签行
+                            number++;
+                            Zimu = Convert.ToChar('A' - 1);
+                            end = 0;
+                            labelBox.Items.Clear();
+                        }
+                    }
+                    continue;
+                }
+                //将标签存进box里
+                labelBox.Items.Add(Zimu + ":" + rng1.Value2.ToString());
+                if (Zimu == 'A')
+                    labelBox.Text = (Zimu + ":" + rng1.Value2.ToString());
+                end = 0;
+            }
+        }
+        //查找
+        private void Searching(Excel.Application excel, Excel.Workbook xBook, char Zimu, int Label_Row, string Search)
+        {
+            int number = 0;
+            int end = 0;
+            int i = 0;
+
+            //从某一列中寻找
+            while (true)
+            {
+                number++;
+                string position = Zimu + Convert.ToString(number);
+                Excel.Range rng1 = excel.get_Range(position, Type.Missing);
+                //如果单元格为空
+                if (rng1.Value2 == null)
+                {
+                    end++;
+                    if (end == 4)
+                    {
+                        if (find == 0)
+                            UnFound();
+                        break;
+                    }
+                    continue;
+                }
+                //如果找到了一样的信息，输出
+                if (Search == Convert.ToString(rng1.Value2))
+                {
+                    if (i == find)
+                    {
+                        Found(excel, number, Label_Row);
+                        find++;
+                        break;
+                    }
+                    i++;
+                }
+                end = 0;
+            }
+            //Console.WriteLine("over");
+        }
+        //找不到该信息（）
+        static void UnFound()
+        {
+            MessageBox.Show("找不到该信息");
+            //Console.WriteLine("找不到该信息");
+        }
+        //找到了，打印信息
+        private void Found(Excel.Application excel, int number, int Label_Row)
+        {
+            int end = 0;
+            char Zimu = 'A';
+            Show.Clear();
+            Show.AppendText("find it!in " + number);
+            //Console.WriteLine("detail:");
+            while (end <= 5)
+            {
+                string position = Zimu + Convert.ToString(number);
+                string label_pos = Zimu + Convert.ToString(Label_Row);
+                Excel.Range label = excel.get_Range(label_pos, Type.Missing);
+                Excel.Range rng1 = excel.get_Range(position, Type.Missing);
+                Zimu++;
+                if (rng1.Value2 == null)
+                {
+                    end++;
+                    continue;
+                }
+                else if (label.Value2 == null)
+                {
+                    Show.AppendText("\r\n" + " " + ":" + rng1.Value2.ToString());
+                }
+                else
+                {
+                    Show.AppendText("\r\n" + label.Value2.ToString() + ":" + rng1.Value2.ToString());
+                }
+                end = 0;
+            }
+        }
+        //结束excel进程
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
+        static void EndOfExcel(Excel.Application excel, Excel.Workbook xBook)
+        {
+            //xBook.Close();
+            excel.Quit();
+            try
+            {
+                if (excel != null)
+                {
+                    int lpdwProcessId;
+                    GetWindowThreadProcessId(new IntPtr(excel.Hwnd), out lpdwProcessId);
+                    System.Diagnostics.Process.GetProcessById(lpdwProcessId).Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Delete Excel Process Error:" + ex.Message);
+            }
+        }
+    }
+}
